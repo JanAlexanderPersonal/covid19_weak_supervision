cfec8921c40ef00b1abd3f0a17ef4cc542ce90b0
Jan Alexander <jan.alexander@verhaert.com>
2021-03-06 20:19:41 +0100

    container runscript update

--- wisenet_container_run.sh
+++ wisenet_container_run.sh
@@ -2,7 +2,7 @@
 # Run the torch container
 docker run --rm --gpus all --net=host --ipc=host -m 24g -e DISPLAY=$DISPLAY \
     -v ${PWD}:/root/space/WiseNet \
-    -v /media/jan/DataStorage/ProjectData/spine_volumes/xVertSeg/Data1:/root/space/data/ \
+    -v /media/jan/DataStorage/ProjectData/covid/:/root/space/data/ \
     -v /media/jan/DataStorage/ProjectData/temp/:/root/space/temp/ \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     --env="QT_X11_NO_MITSHM=1" -it wisenet:torch bash
